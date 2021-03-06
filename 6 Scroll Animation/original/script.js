04cff3c2b3cf8ac9fc846151419804d27282fe12
seanred360 <41050673+seanred360@users.noreply.github.com>
2021-03-06 09:12:58 -0600

    Day 6 Mario edit

--- 6 Scroll Animation/original/script.js
+++ 6 Scroll Animation/original/script.js
@@ -1,7 +1,5 @@
 const boxes = document.querySelectorAll('.box')
-const mario = document.querySelector('.mario')
 
-window.onload = toBottom;
 window.addEventListener('scroll', checkBoxes)
 
 checkBoxes()
@@ -19,10 +17,3 @@ function checkBoxes() {
         }
     })
 }
-
-//https://stackoverflow.com/questions/31173359/can-i-make-the-browser-window-start-at-the-bottom-of-the-page
-function toBottom()
-{
-    window.scrollTo(0, document.body.scrollHeight);
-}
-
