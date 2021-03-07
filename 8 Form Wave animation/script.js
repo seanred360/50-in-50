7ebfd2fa0952f4b61a07bc4257d85d7ff7ea7ebc
seanred360 <41050673+seanred360@users.noreply.github.com>
2021-03-07 11:50:51 -0600

    day 8 added javascript explanation

--- 8 Form Wave animation/script.js
+++ 8 Form Wave animation/script.js
@@ -1,8 +1,16 @@
 const labels = document.querySelectorAll('.form-control label')
 
+console.log(labels)
+
 labels.forEach(label => {
+    // <label></label> = Email
     label.innerHTML = label.innerText
+        // label.innerText = [E, m, a, i, l]
         .split('')
-        .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
+        // [ <span style="transition-delay:50ms">E<span> ..etc.. ]
+        .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`) 
+        // '<span style="transition-delay:0ms">E<span><span style="transition-delay:50ms">m<span>'
+        // makes one big string out of all the spans combined because they are an array, need a string to display
         .join('')
-})
+}) 
+
