e30d6811e6b9e78b0f0391f770fc189ec7c48cdd
seanred360 <41050673+seanred360@users.noreply.github.com>
2021-03-15 19:11:55 -0500

    ringtone download website

--- 9 Sound Board/script.js
+++ 9 Sound Board/script.js
@@ -1,10 +1,29 @@
-const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
+const sounds = ['A Hole Is Calling', 'An Idiot Is Calling', 'FF Victory', 'Fortnite Theme', 'Halo CE Theme', 'MGS Call', 'Pokemon Gym Battle', 'Text Message Man', 'Your Phone is Ringing', 'Mario Bros', 'Sweet Text', 'Whistle']
+const search = document.querySelector('.search')
+const searchBtn = document.querySelector('.searchBtn')
+const input = document.querySelector('.input')
 
 sounds.forEach(sound => {
     const btn = document.createElement('button')
-    btn.classList.add('btn')
+    const icon = document.createElement('i')
+    const txt = document.createElement('span')
 
-    btn.innerText = sound
+    btn.classList.add('btn')
+    icon.classList.add('far')
+    icon.classList.add('fa-play-circle')
+
+    btn.appendChild(icon)
+    btn.appendChild(txt)
+
+    txt.innerText = sound
+
+    const bgNumber = getRandomNumber(25, 1)
+    const redRandom = getRandomNumber(255, 0)
+    const greenRandom = getRandomNumber(255, 0)
+    const blueRandom = getRandomNumber(255, 0)
+
+    btn.style.backgroundImage = `url('resources/backgrounds/bg${bgNumber}.png'), linear-gradient(to left bottom, rgb(${redRandom}, ${greenRandom}, ${blueRandom}), rgb(${255}, ${greenRandom -10}, ${blueRandom - 10}))`
+  
 
     btn.addEventListener('click', () => {
         stopSongs()
@@ -22,3 +41,13 @@ function stopSongs() {
         song.currenTime = 0
     })
 }
+
+function getRandomNumber(max, min) {
+    //https://stackoverflow.com/questions/1527803/generating-random-whole-numbers-in-javascript-in-a-specific-range
+    return Math.round(Math.random() * (max - min) + min)
+}
+
+searchBtn.addEventListener('click', () => {
+    search.classList.toggle('active')
+    input.focus()
+})
