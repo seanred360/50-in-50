462401439e7370c0867f1e2922f3f17dd2857978
seanred360 <41050673+seanred360@users.noreply.github.com>
2021-03-17 17:02:42 -0500

    added catergory buttons
    can browse by genre

--- 17 Movie App/script.js
+++ 17 Movie App/script.js
@@ -1,10 +1,17 @@
-const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=52c0112f1dc3e042da8cc29273e5abfe&page=1' 
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=52c0112f1dc3e042da8cc29273e5abfe&query="'
 
+const API_URL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=52c0112f1dc3e042da8cc29273e5abfe&page=1"
+const API_URL_WILLFERRELL = "https://api.themoviedb.org/3/discover/movie?/discover/movie?with_genres=35&with_cast=23659&sort_by=revenue.desc&api_key=52c0112f1dc3e042da8cc29273e5abfe&page=1"
+const API_URL_DRAMAS = "https://api.themoviedb.org/3/discover/movie?with_genres=18&primary_release_year=2021&api_key=52c0112f1dc3e042da8cc29273e5abfe&page=1"
+
+const nextPage = document.getElementById('next-page')
 const main = document.getElementById('main')
 const form = document.getElementById('form')
 const search = document.getElementById('search')
+const dramas = document.getElementById('dramas')
+
+let pageNumber = 1;
 
 // Get initial movies
 getMovies(API_URL)
@@ -62,4 +69,14 @@ form.addEventListener('submit', (e) => {
     } else {
         window.location.reload()
     }
+})
+
+nextPage.addEventListener('click', () => {
+    pageNumber++
+    getMovies(API_URL.slice(0, -1) + pageNumber)
+})
+
+dramas.addEventListener('click', () => {
+    pageNumber = 1
+    getMovies(API_URL_DRAMAS)
 })
