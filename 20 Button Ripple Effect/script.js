42a5e5a44a452e8cb1e68fb69686137d01c03422
seanred360 <41050673+seanred360@users.noreply.github.com>
2021-03-22 10:45:25 -0500

    day 20 ripple button
    rewrote the code from memory

--- 20 Button Ripple Effect/script.js
+++ 20 Button Ripple Effect/script.js
@@ -1,23 +1,50 @@
 const buttons = document.querySelectorAll('.ripple')
 
-buttons.forEach(button => {
+buttons.forEach((button) => {
     button.addEventListener('click', function(evnt) {
         const x = evnt.clientX
         const y = evnt.clientY
-        
-        const buttonTop = evnt.target.offsetTop
-        const buttonLeft = evnt.target.offsetLeft
 
-        const xInside = x - buttonLeft
-        const yInside = y - buttonTop
+        console.log(x, y)
 
         const circle = document.createElement('span')
         circle.classList.add('circle')
-        circle.style.top = yInside + 'px'
-        circle.style.left = xInside + 'px'
 
-        this.appendChild(circle)
+        const innerX = x - button.offsetLeft
+        const innerY = y - button.offsetTop
+
+        console.log(innerX, innerY)
+
+        circle.style.left = innerX + 'px'
+        circle.style.top = innerY + 'px'
+
+        button.appendChild(circle)
 
         setTimeout(() => circle.remove(), 500)
     })
 })
+
+// solution
+// const buttons = document.querySelectorAll('.ripple')
+
+// buttons.forEach(button => {
+//     button.addEventListener('click', function(evnt) {
+//         const x = evnt.clientX
+//         const y = evnt.clientY
+        
+//         const buttonTop = evnt.target.offsetTop
+//         const buttonLeft = evnt.target.offsetLeft
+
+//         const xInside = x - buttonLeft
+//         const yInside = y - buttonTop
+
+//         const circle = document.createElement('span')
+//         circle.classList.add('circle')
+//         circle.style.top = yInside + 'px'
+//         circle.style.left = xInside + 'px'
+
+//         this.appendChild(circle)
+
+//         setTimeout(() => circle.remove(), 500)
+//     })
+// })
